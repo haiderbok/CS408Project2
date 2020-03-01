c6ab096c1a6c4fc7f86881e14047166fec316cc5
NicholasMeier <nmeier@purdue.edu>
2020-03-01 15:27:22 -0500

    added skeleton to Pi.java

--- Pi.java
+++ Pi.java
@@ -4,11 +4,26 @@
  *
  */
 
+//TODO 
+// Create method to convert callgraph input into set of data
+  /* Data format : Func. <int memid, str name, int uses, map<list of calls>, map<list of pairs>> */
+// Create method to analyze the parsed graph, iterate through function list and generate pairs
+ /* (IE for func A(), B() if A() and B() are together 5 times, and B() is called 7 times, then generate
+    a value 5/7 for confidence of there being a logic bug. More details in doc page 2,3*/
+
 public class Pi{
 	public static void main(String args[]){
 		Scanner in = new Scanner(System.in);
 		while (in.hasNextLine()){
 			String s = in.nextLine();	
 		}
+	}
+
+	public void parseCallGraph(){
+		//todo, see above	
+	}
+
+	public void analyzeData(){
+		//todo, see above
 	}
 }
