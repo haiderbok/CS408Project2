406db88bd811e2ecc365ee6e651fc2f4ca993088
NicholasMeier <nmeier@purdue.edu>
2020-03-01 14:29:49 -0500

    changed dir struct, added base src file, made callgraph names standard

---
+++ Pi.java
@@ -0,0 +1,14 @@
+/*
+ *  Project 2 - CS408 
+ *  Spring 2020
+ *
+ */
+
+public class Pi{
+	public static void main(String args[]){
+		Scanner in = new Scanner(System.in);
+		while (in.hasNextLine()){
+			String s = in.nextLine();	
+		}
+	}
+}
