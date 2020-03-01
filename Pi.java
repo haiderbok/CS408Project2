a96f3ec91533515860b1adba6bd3a61f019389c3
nicholasmeier <nmeier@purdue.edu>
2020-03-01 15:40:59 -0500

    created func datastructure, need to add parsing

--- Pi.java
+++ Pi.java
@@ -1,3 +1,5 @@
+import javafx.util.Pair;
+import java.util.ArrayList;
 /*
  *  Project 2 - CS408 
  *  Spring 2020
@@ -6,24 +8,30 @@
 
 //TODO 
 // Create method to convert callgraph input into set of data
-  /* Data format : Func. <int memid, str name, int uses, map<list of calls>, map<list of pairs>> */
+/* Data format : Func. <int memid, str name, int uses, map<list of calls>, map<list of pairs>> */
 // Create method to analyze the parsed graph, iterate through function list and generate pairs
- /* (IE for func A(), B() if A() and B() are together 5 times, and B() is called 7 times, then generate
-    a value 5/7 for confidence of there being a logic bug. More details in doc page 2,3*/
+/* (IE for func A(), B() if A() and B() are together 5 times, and B() is called 7 times, then generate
+   a value 5/7 for confidence of there being a logic bug. More details in doc page 2,3*/
+class func {
+	int memid;
+	String name;
+	int uses;
 
-public class Pi{
-	public static void main(String args[]){
-		Scanner in = new Scanner(System.in);
-		while (in.hasNextLine()){
-			String s = in.nextLine();	
-		}
+	// Use memid (hex) for these lists
+	ArrayList<Integer> calls;
+	ArrayList<Pair<Integer, Integer>> callPairs;
+}
+
+public class Pi {
+	public static void main(String args[]) {
+		parseCallGraph();
 	}
 
-	public void parseCallGraph(){
-		//todo, see above	
+	public static void parseCallGraph() {
+
 	}
 
-	public void analyzeData(){
-		//todo, see above
+	public static void analyzeData() {
+		// todo, see above
 	}
 }
